ce8dd990573f1ace3c8afea7b475f71c9aeb2e02
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-06-26 18:09:32 -0400

    contact form added

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const menu = document.getElementById('menu')
+const mainbox = document.getElementById('mainbox')
 
 function openFunction(){
     menu.style.width="300px"
+    mainbox.style.marginLeft="150px"
 }
 
 function closeFunction(){
     menu.style.width="0px"
+    mainbox.style.marginLeft="0px"
 }
